0c2d3225c549acb8ce0b07d51c0820a5bbbb260b
Demanrusss <deman.russs@gmail.com>
2022-09-22 00:26:27 +0600

    Problem Set 7 (part 1)

---
+++ Problem_Set_7/4.sql
@@ -0,0 +1,3 @@
+SELECT COUNT(movie_id) AS [Number of movies]
+FROM ratings
+WHERE ratings.rating = 10.0
---
+++ Problem_Set_7/6.sql
@@ -0,0 +1,4 @@
+SELECT AVG(ratings.rating) AS [Average rating]
+FROM ratings, movies
+WHERE movies.year = 2012
+AND movies.id = ratings.movie_id
