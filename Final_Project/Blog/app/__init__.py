6a756e050f5c4114f810d6ee0e75062c6fc2a8fb
Demanrusss <deman.russs@gmail.com>
2022-11-22 23:58:05 +0600

    Created db and connection scripts

--- Final_Project/Blog/app/__init__.py
+++ Final_Project/Blog/app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 
 csrf = CSRFProtect()
 
 app = Flask(__name__)
 app.config.from_object('config')
+db = SQLAlchemy(app)
 csrf.init_app(app)
 
-from Blog import views
+from app import views, models
